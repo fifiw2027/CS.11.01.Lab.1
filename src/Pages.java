cd67802e422348957c01a868a4d194af054ef881
fifiw2027 <fifiw2027@student.cis.edu.hk>
2024-08-28 09:13:35 +0800

    Update Pages.java
    Pages done!

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scanner.nextInt();
+        int page = 100 - age;
+        String pageToString = "" + page;
+        String result = age + "-year olds should read at least " + page + " pages before giving up on the book.";
+        System.out.println(result);
     }
 }
