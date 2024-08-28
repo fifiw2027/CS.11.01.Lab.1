237db5873f0132cb4d8eb013fbdca6ec86f55b2b
fifiw2027 <fifiw2027@student.cis.edu.hk>
2024-08-28 09:14:11 +0800

    Update Dateable.java
    Dateable done!

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scanner.nextInt();
+        int age2 = age/2 + 7;
+        String result = age + "-year olds should date someone who is at least " + age2 + " years old.";
+        System.out.println(result);
     }
 }
